baf0b462cc1c4eb46cd96bcabd9f1dee806bd056
Piotr Krupa <piotrkrupa06@gmail.com>
2018-08-30 21:16:17 +0200

    Added comments

--- src/providers/hoverProvider.ts
+++ src/providers/hoverProvider.ts
@@ -9,8 +9,10 @@ export class HoverProvider implements vscode.HoverProvider
     {
         let result = "";
 
+        // foreach every text in segment
         dialogLine.txtSegment.texts.forEach(element => {
 
+            // find character from id
             let character = dialogLine.jsonSegment.characters.find((character : Character) => {
                 let result = character.texts.find((text) => {
                     return text.ID.toString() === element.ID;
@@ -19,21 +21,25 @@ export class HoverProvider implements vscode.HoverProvider
                 return result !== undefined;
             });
 
+            // if found character
             if (character !== undefined)
             {
                 result += "**" + character.name + "**";
             }
-            else
+            else // if not
             {
                 result += "[" + element.ID + "]";  
             }
 
+            // add text what character said
             result += ": " + element.text + "\n\n";
         });
 
+        // get link to `dialog.json` and `dialog.txt`
         let jsonPath = Dialog.getInstance().getDialogPath() + Dialog.JSON_FILENAME;
         let textPath = Dialog.getInstance().getDialogPath() + Dialog.TXT_FILENAME;
 
+        // add link to message
         result += "[dialog.json](file:" + jsonPath + ")";
         result += " | ";
         result += "[dialog.txt](file:" + textPath + ")";
@@ -42,23 +48,33 @@ export class HoverProvider implements vscode.HoverProvider
 
     provideHover(doc: vscode.TextDocument, pos: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> 
     {
+        // get dialog instance and update
         let dialog = Dialog.getInstance();
         dialog.update(doc);
         
+        // create regex
         const regex = /dialog:run\([\t ]*\"(.*)\"[\t ]*\)/;
 
+        // get line from position
         let line = doc.lineAt(pos.line).text;
+        
+        // parse current line
         let result = regex.exec(line);
+
         if (result !== null) 
         {
+            // get id
             let id = result[1];
 
+            // get dialog line from id
             let dialogLine = dialog.getDialogLine(id);
 
             if (dialogLine !== undefined)
             {
+                // generate hover message
                 let str = HoverProvider.getDialogText(dialogLine);
 
+                // show hover message
                 return new vscode.Hover(str);
             }
 
